fd200bd105afe8f02c954226e131c2028587d77a
ho757uff <robindebessel.pro@gmail.com>
2023-12-07 18:00:52 +0100

    added users_controller and dependencies

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      flash[:alert] = "L'utilisateur que vous cherchez n'existe pas."
+      redirect_to user_path(current_user)
+    elsif current_user != @user
+      flash[:notice] = "Vous n'êtes pas autorisé à accéder à ce profil."
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :current_password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,33 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # Defines the root path route ("/")
+  root "static_pages#home"
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", :as => :rails_health_check
+
+  resources :locations
+  resources :events
+
+  # Static pages
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
+
+  # # Users show routes:
+  resources :users do
+    #   resources :guest_list, only: [:show, :update]
+    #   resources :menu, only: [:show, :update]
+    #   resources :wishlist, only: [:show, :update]
+    #   resources :photos, only: [:index, :show]
+    #   resources :guest_book, only: [:show, :update]
+    #   resources :fund, only: [:show, :update]
+  end
   # resources :guest_lists
   # resources :menus
   # resources :wishlists
   # resources :funds
   # resources :guest_books
   # resources :photos
-  devise_for :users
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", :as => :rails_health_check
-
-  # Defines the root path route ("/")
-  root "static_pages#home"
-
-  resources :locations
-  resources :events
-
-  # # Users show routes:
-  # resources :users do
-  #   resources :guest_list, only: [:show, :update]
-  #   resources :menu, only: [:show, :update]
-  #   resources :wishlist, only: [:show, :update]
-  #   resources :photos, only: [:index, :show]
-  #   resources :guest_book, only: [:show, :update]
-  #   resources :fund, only: [:show, :update]
-  # end
 end
