7f3ccdc23ecf05800828c8306752338580624c4b
ho757uff <robindebessel.pro@gmail.com>
2023-12-08 15:59:32 +0100

    front update - buttons and other fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", :as => :rails_health_check
+  post "up" => "rails/health#show", :as => :rails_health_check
 
   resources :locations
   resources :events
@@ -16,12 +16,12 @@ Rails.application.routes.draw do
   end
 
   # Static pages
-  get "about", to: "static_pages#about"
-  get "contact", to: "static_pages#contact"
-  get "feedback", to: "static_pages#feedback"
-  get "terms", to: "static_pages#terms"
-  get "privacy", to: "static_pages#privacy"
-  get "faq", to: "static_pages#faq"
+  post "about", to: "static_pages#about"
+  post "contact", to: "static_pages#contact"
+  post "feedback", to: "static_pages#feedback"
+  post "terms", to: "static_pages#terms"
+  post "privacy", to: "static_pages#privacy"
+  post "faq", to: "static_pages#faq"
 
   # # Users show routes:
   resources :users do
