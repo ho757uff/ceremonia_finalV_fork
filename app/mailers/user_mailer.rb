43e6a81878d0e935f48af8de0771321042a592a3
mathieu.amacher <amacher_mathieu@hotmail.com>
2023-12-11 14:34:23 +0100

    mailer en production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user
         
-        @url = 'http://monsite.fr/login'
+        @url = 'https://the-wedding-project-1b83c5344eda.herokuapp.com/users/sign_in'
 
         mail(to: @user.email, subject: "Votre inscription sur Ceremonia")
     end
